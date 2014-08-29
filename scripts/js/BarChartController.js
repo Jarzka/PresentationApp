823c4bc8227c002fd9c3641b94976b64c5a79a57
Jari Hanhela <jarihanhela@gmail.com>
2014-08-29 17:04:05 +0300

    One presentation can now contain multiple bar charts.

--- scripts/js/BarChartController.js
+++ scripts/js/BarChartController.js
@@ -8,10 +8,6 @@ function BarChartController() {
 
     // Private
 
-    var bars = {};
-    var highestBar = {};
-    var highestBarIndex = 0;
-    var lowestBar = {};
     var highestBarHeightPx = 270;
 
     function convertBarChartsToStandardHTML() {
@@ -34,10 +30,17 @@ function BarChartController() {
 
         var chartContent = $("<div class='chart-content'></div>");
         $(".bar-chart").append(chartContent);
-        $(".bar-chart .bar").detach().appendTo(".bar-chart .chart-content");
+
+        $(".bar-chart .bar").each(function() {
+            var parentChartContent = $(this).parents(".bar-chart").children(".chart-content");
+            $(this).detach().appendTo(parentChartContent);
+        });
     }
 
-    function findHighestAndLowestBars() {
+    /** @param bars jQuery DOM object presenting bars */
+    function findHighestBar(bars) {
+        var highestBar = {};
+
         bars.each(function(index) {
             if (index == 0) {
                 highestBar = $(this);
@@ -49,38 +52,44 @@ function BarChartController() {
             }
         });
 
-        bars.each(function(index) {
-            if (index == 0) {
-                lowestBar = $(this);
-            } else {
-                if ($(this).attr("value") < lowestBar.attr("value")) {
-                    lowestBar = $(this);
-                }
-            }
-        });
+        return highestBar;
     }
 
     function positionChartsOnXAxis() {
-        // Position bars on the x-axis.
-        bars.each(function(index) {
-            $(this).css("left", index * 100 + 25);
-        });
+        $(".chart-content").each(function() {
+            // Position bars on the x-axis.
+            $(this).children(".bar").each(function(index) {
+                $(this).css("left", index * 100 + 25);
+            });
+        })
     }
 
     function setBarHeights() {
-        // Set highest bar value.
-        highestBar.css("height", highestBarHeightPx);
+        $(".chart-content").each(function() {
+            // Set highest bar value.
+            var highestBar = findHighestBar($(this).children(".bar"));
+            highestBar.css("height", highestBarHeightPx);
 
-        // All other chart height values are set relative to the highest bar value.
-        bars.each(function(index) {
-            if (index != highestBarIndex) {
-                $(this).css("height", ($(this).attr("value") / highestBar.attr("value")) * highestBarHeightPx);
-            }
+            // Set all other bar heights relative to the highest bar value.
+            $(this).children(".bar").each(function(index) {
+                if (index != highestBarIndex) {
+                    if ($(this).attr("value") >= 0) {
+                        $(this).css("height", ($(this).attr("value") / highestBar.attr("value")) * highestBarHeightPx);
+                    } else {
+                        // Currently there is no good support for negative values. Hide the bar.
+                        $(this).css("height", 0);
+                    }
+                }
+            });
         });
+
     }
 
     function setBarChartSize() {
-        $(".bar-chart").css("width", bars.length * 100 + 100);
+        $(".bar-chart").each(function() {
+            var barsInThisChart = $(this).find(".bar");
+            $(this).css("width", barsInThisChart.length * 100 + 100); // TODO HARDCODED VALUES
+        });
     }
 
     function colorBarChartBars() {
@@ -90,15 +99,18 @@ function BarChartController() {
     }
 
     function addLabels() {
-        bars.each(function(index) {
-            var label = $("<p class='label'>" + $(this).attr("name") + "</p>");
-            label.css("left", index * 100 + 110); // TODO HARDOCDED VALUES
-            $(this).parents(".bar-chart").append(label);
-        });
+        $(".bar-chart").each(function() {
+            $(this).find(".bar").each(function(index) {
+                var label = $("<p class='label'>" + $(this).attr("name") + "</p>");
+                label.css("left", index * 100 + 110); // TODO HARDOCDED VALUES
+                $(this).parents(".bar-chart").append(label);
+            });
+        })
+
     }
 
     function addBarNumbers() {
-        bars.each(function(index) {
+        $(".bar").each(function(index) {
             var label = $("<p class='value'>" + $(this).attr("value") + "</p>");
             $(this).append(label);
         });
@@ -106,8 +118,8 @@ function BarChartController() {
 
     /** If the bar is very low in height, move the number above the bar*/
     function fixLowBarNumbers() {
-        bars.each(function() {
-            if ($(this).height() <= 20) {
+        $(".bar").each(function() {
+            if ($(this).height() <= 30) {
                 $(this).children(".value").each(function() {
                    $(this).css("top", -30);
                 });
@@ -117,37 +129,40 @@ function BarChartController() {
 
     /** Set the bar chart height so that all labels are shown. */
     function fixBarChartHeight() {
-        // Find the label with the highest height.
-        var highestLabel = {};
-        $(".bar-chart .label").each(function(index) {
-            if (index == 0) {
-                highestLabel = $(this);
-            } else {
-                if ($(this).height() > highestLabel.height()) {
+        $(".bar-chart").each(function() {
+            // Find the label with the highest height.
+            var highestLabel = {};
+            $(this).find(".label").each(function(index) {
+                if (index == 0) {
                     highestLabel = $(this);
+                } else {
+                    if ($(this).height() > highestLabel.height()) {
+                        highestLabel = $(this);
+                    }
                 }
-            }
-        });
+            });
 
-        var parentBarChart = highestLabel.parents(".bar-chart");
-        parentBarChart.css("height", parentBarChart.height() + highestLabel.height());
+            $(this).css("height", $(this).height() + highestLabel.height());
+        });
     }
 
     function addAxes() {
-        var barCharts = $(".bar-chart");
-        var axisHighestValue = $("<p class='axis'>" + highestBar.attr("value") + "</p>");
-        var axisMiddleValue = $("<p class='axis'>" + (highestBar.attr("value") / 2) + "</p>");
-        var axisLowestValue = $("<p class='axis'>" + 0 + "</p>");
-        barCharts.append(axisHighestValue);
-        barCharts.append(axisMiddleValue);
-        barCharts.append(axisLowestValue);
+        $(".bar-chart").each(function() {
+            var highestBar = findHighestBar($(this).find(".bar"));
+            var axisHighestValue = $("<p class='axis'>" + highestBar.attr("value") + "</p>");
+            var axisMiddleValue = $("<p class='axis'>" + (highestBar.attr("value") / 2) + "</p>");
+            var axisLowestValue = $("<p class='axis'>" + 0 + "</p>");
+            $(this).append(axisHighestValue);
+            $(this).append(axisMiddleValue);
+            $(this).append(axisLowestValue);
 
-        axisHighestValue.css("left", 10);
-        axisHighestValue.css("top", 3);
-        axisMiddleValue.css("left", 10);
-        axisMiddleValue.css("top", 132); // TODO HARDCODED VALUE
-        axisLowestValue.css("left", 10);
-        axisLowestValue.css("top", 266); // TODO HARDCODED VALE
+            axisHighestValue.css("left", 10);
+            axisHighestValue.css("top", 3);
+            axisMiddleValue.css("left", 10);
+            axisMiddleValue.css("top", 132); // TODO HARDCODED VALUE
+            axisLowestValue.css("left", 10);
+            axisLowestValue.css("top", 266); // TODO HARDCODED VALE
+        });
     }
 
     function constructBarCharts() {
@@ -155,16 +170,15 @@ function BarChartController() {
         bars = $(".bar-chart .bar");
 
         if (bars.length > 0) {
-            findHighestAndLowestBars();
             positionChartsOnXAxis();
             setBarHeights();
             setBarChartSize();
             colorBarChartBars();
             addLabels();
+            fixBarChartHeight();
             addBarNumbers();
             fixLowBarNumbers();
             addAxes();
-            fixBarChartHeight();
         }
     }
 
